29c503d2d755f5a6b61dde0eeaf2217fd4dba4b2
varnika <varnika@ibm.com>
2023-11-15 18:21:56 -0500

    GPT-4 sometimes returns multiple queries, so adding handling for that here

--- initial_pipeline.py
+++ initial_pipeline.py
@@ -4,8 +4,8 @@ import sqlite3
 import time
 
 # following https://platform.openai.com/examples/default-sql-translate and https://stackoverflow.com/questions/76053920/how-do-i-extract-only-code-content-from-chat-gpt-response
-# adding a system promot let's chatgpt know what to return (in this case, a sql query, but we can also ask it to include explanations as well as it did in the UI)
-system_knowledge = """Given the following SQL tables, your job is to write queries given a user’s request.
+# adding a system prompt let's chatgpt know what to return (in this case, a sql query, but we can also ask it to include explanations as well as it did in the UI)
+system_knowledge = """Given the following SQL tables schemas and its example row (SELECT * FROM table limit 1;), your job is to write queries given a user’s request.
 CREATE TABLE routes (
   route_id INTEGER,
   line_id TEXT,
@@ -16,6 +16,7 @@ CREATE TABLE routes (
   route_name TEXT,
   PRIMARY KEY (route_id)
 );
+Example row for routes: 0|blue|place-wondl|place-bomnl|0|West|Wonderland to Bowdoin
 CREATE TABLE gated_station_entries (
   service_date TEXT,
   time TEXT,
@@ -24,16 +25,19 @@ CREATE TABLE gated_station_entries (
   gated_entries REAL,
   PRIMARY KEY (service_date, time, station_id, line_id)
 );
+Example row for gatest_station_entries: 2022-05-31|00:00:00|place-alfcl|red|5.0
 CREATE TABLE lines (
   line_id TEXT,
   line_name TEXT,
   PRIMARY KEY (line_id)
 );
+Example row for lines: blue|Blue Line
 CREATE TABLE stations (
   station_id TEXT,
   station_name TEXT,
   PRIMARY KEY (station_id)
 );
+Example row for stations: place-aport|Airport
 CREATE TABLE station_orders (
   route_id INTEGER,
   station_id TEXT,
@@ -41,6 +45,7 @@ CREATE TABLE station_orders (
   distance_from_last_station_miles REAL,
   PRIMARY KEY (route_id, station_id)
 );
+Example row for station_orders: 0|place-wondl|1|0.0
 CREATE TABLE rail_ridership (
   season TEXT,
   line_id TEXT,
@@ -55,6 +60,7 @@ CREATE TABLE rail_ridership (
   average_flow INTEGER,
   PRIMARY KEY (season, line_id, direction, time_period_id, station_id)
 );
+Example row for rail_ridership: Fall 2017|blue|0|time_period_02|place-orhte|37523|1211|82|458|15|1987
 CREATE TABLE time_periods (
   time_period_id TEXT,
   day_type TEXT,
@@ -63,12 +69,16 @@ CREATE TABLE time_periods (
   period_end_time TEXT,
   PRIMARY KEY (time_period_id)
 );
+Example row for time_periods: time_period_01|weekday|VERY_EARLY_MORNING|3:00:00|05:59:59 
 """
 
+# Tried including "Do not attempt to format on the result apart from selecting the correct columns. Selecting from the table gives the expected display output regardless of what the prompt says."
+# but it didn't work
+
 # need to change this based on the prompt you are trying for
-user_prompt = """Find all stations which are at least 1 mile away from the previous station. Report the station ID, route ID, and distance
-(in miles) to the previous station, sorted by decreasing distance. Break ties in distance by sorting by route ID and then
-station ID, both in ascending order."""
+user_prompt = """Report the historical total ons on weekdays between 4:00 PM and 6:29 PM (time period 6) per season for the “Kendall/MIT”
+Red Line station. Report the season, line ID, direction, and total ons, sorted by the season and direction in ascending
+order."""
 
 print("total number of tokens in the current prompt:".upper(), num_tokens_from_string(system_knowledge + user_prompt))
 
@@ -79,9 +89,13 @@ end = time.time()
 print("model's answer:".upper(), answer, "\n(took", end-start, "seconds)\n")
 print("===================================\n")
 
-test_query = answer[answer.find("```sql") + len("```sql"):]
-test_query = test_query[:test_query.find("```")]
-# test_query = answer.removeprefix("```sql").removesuffix("```")
+# sometimes gpt-4 provides multiple queries with explanations, we want to extract all sql queries presented
+answer_copy = answer
+test_queries = []
+while answer_copy.find("```sql") != -1:
+   answer_copy = answer_copy[answer_copy.find("```sql") + len("```sql"):]
+   test_queries.append(answer_copy[:answer_copy.find("```")])
+   answer_copy = answer_copy[answer_copy.find("```") + len("```"):]
 
 mbta_db = '../lab0/mbta.sqlite' # replace with your path to mbta
 conn = sqlite3.connect(mbta_db)
@@ -89,23 +103,27 @@ c = conn.cursor()
 
 # replace with the correct query you are looking for based on the prompt
 target_query = """
-select station_id, route_id, distance_from_last_station_miles as distance from station_orders where distance >= 1.0 order by distance desc, route_id, station_id;
+select season, line_id, direction, total_ons from rail_ridership as r join time_periods as t on r.time_period_id = t.time_period_id join stations as s on r.station_id = s.station_id where day_type = "weekday" and period_start_time between '16:00:00' AND '18:30:00' and station_name = "Kendall/MIT" order by season, direction;
 """
 
 try:
-    print("running sql query from model:".upper(), test_query)
-    print()
-    print("===================================\n")
-    test_result = c.execute(test_query).fetchall()
-    print("results from sql query from model".upper())
-    print(test_result)
-    print()
     target_result = c.execute(target_query).fetchall()
+    # comment until next 3 lines if don't want to see results
     print("results from expected query".upper())
     print(target_result)
     print()
     print("===================================\n")
-    print("comparing query accuracy with target".upper())
-    print(compare_accuracy(test_result, target_result))
+    print("running sql query(s) from model:".upper())
+    for t in test_queries:
+        print(t)
+        test_result = c.execute(t).fetchall()
+        # comment until next 3 lines if don't want to see results
+        print("results from sql query above".upper())
+        print(test_result)
+        print()
+        print("comparing query accuracy with target".upper())
+        print(compare_accuracy(test_result, target_result))
+        print()
+        print("===================================\n")
 except Exception as e:
     print("error running sql query: ".upper(), e)
